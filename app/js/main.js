106c165e0a1e14f96ae8ef8b4b0bbf85aaa85742
Olesya <nuisance72@gmail.com>
2023-02-20 16:32:55 +0300

    first commit

---
+++ app/js/main.js
@@ -0,0 +1,15 @@
+$(".header__slider").slick({
+    infinite: true,
+    fade: true,
+    nextArrow: '<img class="slider-arrows slider-arrows-left" src="./image/header_arrow.svg" alt="стрелка">',
+    prevArrow: '<img class="slider-arrows slider-arrows-right" src="./image/header_arrow_pprew.svg" alt="стрелка">',
+
+    //дотсы в хедере
+    dots: true,
+    appendDots: '.slider-dots',
+    customPaging: function(slider, i) {
+        const slide = slider.$slides[i];
+        const title = $(slide).find('.slider-item__info-title').text();
+        return `<button type="button"><span class='slider-dots__number'>0${i + 1}</span><span>${title}</span></button>`;
+    },
+})
